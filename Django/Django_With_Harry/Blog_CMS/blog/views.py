fbbf406b9a41d754ad0ea4a386e0fbb0e1297280
Ariel Lorusso <ariellorusso@gmail.com>
2024-04-03 19:13:27 -0300

    Made a list of Django Courses & content Ordered all tutorials in a same folder

--- Django/Django_With_Harry/Blog_CMS/blog/views.py
+++ Django/Django_With_Harry/Blog_CMS/blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render ,HttpResponse
+from blog.models import Blog
 
-# Create your views here.
 def home(request):
     return render(request, 'index.html')
 
 def blog(request):
-    return render(request, 'blogpost.html')
+    blogs = Blog.objects.all()
+    context = {'blogs': blogs }
+    return render(request, 'bloghome.html', context)
 
 def blogpost(request,slug):
     return render(request, 'blogpost.html')
